b3ceaf6662d6010f472b61760474e7e320e125f8
lauro <laurolyra@gmail.com>
2020-06-08 17:13:49 -0300

    Lauro MongoDB Aggregations

---
+++ desafios/desafio01.js
@@ -0,0 +1,24 @@
+use aggregations;
+db.movies.aggregate([
+  {
+    $match: {
+      'imdb.rating': { $gte: 7 },
+      genres: { $nin: ['Crime', 'Horror'] },
+      rated: { $in: ['PG', 'G'] },
+      languages: { $all: ['English', 'Spanish'] },
+    }}
+]);
+
+// counting movies:
+db.movies.aggregate([
+  {
+    $match: {
+      'imdb.rating': { $gte: 7 },
+      genres: { $nin: ['Crime', 'Horror'] },
+      rated: { $in: ['PG', 'G'] },
+      languages: { $all: ['English', 'Spanish'] },
+    }},
+    {
+      $count: "movies"
+    }
+]);
