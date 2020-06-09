3ae8f892a078e452dd62fa135ad645e0d81cab35
guicgs <etohlif@protonmail.com>
2020-06-08 22:33:31 -0300

    desafio 11 concluído

---
+++ desafio11.js
@@ -0,0 +1,11 @@
+use aggregations;
+
+db.trips.aggregate([
+  {
+    $group: {
+      _id: { $dayOfWeek: "$startTime" },
+      total: { $sum: 1 }
+    }
+  },
+  { $project: { _id: 0, diaDaSemana: "$_id", total: 1 } }
+]);
