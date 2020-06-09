d3dc4cf37c1c2c041b4260ab98942dafd3f1b1ab
eddyeboy27 <lipe0503@gmail.com>
2020-06-09 00:16:36 -0300

    Finalizado req6

---
+++ desafio6.js
@@ -0,0 +1,31 @@
+use aggregations;
+
+db.movies.aggregate([
+  {
+    $match: {
+      "awards": /^Won.*Oscar/
+    }
+  },
+  {
+    $group: {
+      _id: null,
+      "maior_rating": {
+        $max: "$imdb.rating"
+      },
+      "menor_rating": {
+        $min: "$imdb.rating"
+      },
+      "media_rating": {
+        $avg: "$imdb.rating"
+      },
+      "desvio_padrao": {
+        $stdDevPop: "$imdb.rating"
+      }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+    }
+  }
+]).pretty();
