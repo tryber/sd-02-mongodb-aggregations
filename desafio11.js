87514f483c88b5814bdac9b72c2788bf0e700a88
guicgs <etohlif@protonmail.com>
2020-06-08 22:51:56 -0300

    desafio 12 concluído

--- desafio11.js
+++ desafio11.js
@@ -7,5 +7,7 @@ db.trips.aggregate([
       total: { $sum: 1 }
     }
   },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
   { $project: { _id: 0, diaDaSemana: "$_id", total: 1 } }
 ]);
---
+++ desafio12.js
@@ -0,0 +1,16 @@
+use aggregations;
+
+db.trips.aggregate([
+  {
+    $group: {
+      _id: { 
+        diaDaSemana: { $dayOfWeek: "$startTime" }, 
+        nomeEstacao: "$startStationName" 
+      },
+      total: { $sum: 1 }
+    }
+  },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+  { $project: { _id: 0, nomeEstacao: "$_id.nomeEstacao", total: 1 } }
+]);
