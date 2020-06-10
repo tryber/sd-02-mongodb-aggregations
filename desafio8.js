0c8bd1a119800fed839efc7fb075a85abb24034d
eddyeboy27 <lipe0503@gmail.com>
2020-06-09 23:21:28 -0300

    Finalizado req8

--- desafio8.js
+++ desafio8.js
@@ -1,5 +1,41 @@
 use aggregations;
 
 db.air_alliances.aggregate([
-  
-])
+  {
+    $unwind: "$airlines"
+  },
+  {
+    $lookup: {
+      from: "air_routes",
+      let: { air_lines: "$airlines" },
+      pipeline: [
+        {
+          $match: {
+            $expr: {
+              $eq: [ "$airline.name", "$$air_lines" ]
+            },
+            "airplane": { $in: [ "747", "380" ] }
+          }
+        }
+      ],
+      as: "alliance_routes"
+    }
+  },
+  {
+    $match: { "alliance_routes": { $ne: [] } }
+  },
+  {
+    $unwind: "$alliance_routes"
+  },
+  {
+    $group: {
+      _id: "$name",
+      "totalRotas": { $sum: 1 }
+    }
+  },
+  {
+    $sort: {
+      "totalRotas": -1
+    }
+  }
+]).pretty();
