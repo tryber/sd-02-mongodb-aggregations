58bd851cf87af78a584c5d7b15303a8bb5047279
guicgs <etohlif@protonmail.com>
2020-06-10 14:31:38 -0300

    desafio 12 corrigido

--- desafio12.js
+++ desafio12.js
@@ -1,16 +1,40 @@
 use aggregations;
 
 db.trips.aggregate([
+  { $match: { $expr: { $eq: [{ $dayOfWeek: "$startTime" }, 5] } } },
   {
     $group: {
-      _id: { 
-        diaDaSemana: { $dayOfWeek: "$startTime" }, 
-        nomeEstacao: "$startStationName" 
-      },
+      _id: "$startStationName",
       total: { $sum: 1 }
     }
   },
   { $sort: { total: -1 } },
   { $limit: 1 },
-  { $project: { _id: 0, nomeEstacao: "$_id.nomeEstacao", total: 1 } }
+  { $project: { _id: 0, nomeEstacao: "$_id", total: 1 } }
+]);
+
+// outra forma
+const most_trips_day = db.trips.aggregate([
+  {
+    $group: {
+      _id: { $dayOfWeek: "$startTime" },
+      total: { $sum: 1 }
+    }
+  },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+  { $project: { _id: 0, diaDaSemana: "$_id", total: 1 } }
+]).toArray()[0].diaDaSemana;
+
+db.trips.aggregate([
+  { $match: { $expr: { $eq: [{ $dayOfWeek: "$startTime" }, most_trips_day] } } },
+  {
+    $group: {
+      _id: "$startStationName",
+      total: { $sum: 1 }
+    }
+  },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+  { $project: { _id: 0, nomeEstacao: "$_id", total: 1 } }
 ]);
