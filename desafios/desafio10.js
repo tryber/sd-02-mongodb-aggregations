207b4224bf0f4c48a6fee531e30bf51c657dc80d
richardwellerson <richardwellerson@gmail.com>
2020-06-20 14:33:47 -0300

    Desafio 10 feito

---
+++ desafios/desafio10.js
@@ -0,0 +1,39 @@
+use aggregations;
+
+db.trips.aggregate(
+  [
+    {
+      $group: {
+        _id: "$usertype",
+        media: {
+          $avg: {
+            $subtract: [
+              "$stopTime", "$startTime"
+            ]
+          }
+        }
+      }
+    },
+    {
+      $project: {
+        _id: 0,
+        tipo: "$_id",
+        duracaoMedia: {
+          $round: [
+            {
+              $divide: [
+                "$media", 1000 * 3600
+              ]
+            },
+            2
+          ]
+        },
+      },
+    },
+    {
+      $sort: {
+        duracaoMedia: -1
+      }
+    }
+  ]
+);
