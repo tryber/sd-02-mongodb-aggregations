89ad1945d9e81d112df87307eb8cd3fbbc79c6f0
miguel-miranda <ayandram7727@gmail.com>
2020-06-20 21:33:40 -0300

    desafio 8

---
+++ desafio8.js
@@ -0,0 +1,29 @@
+use aggregations;
+
+db.air_alliances.aggregate([
+  { $unwind: "$airlines" },
+  {
+    $lookup: {
+      from: "air_routes",
+      let: { airlineAlliance: "$airlines" },
+      pipeline: [
+        {
+          $match: {
+            $expr: { $eq: ["$airline.name", "$$airlineAlliance"] },
+            airplane: { $in: ['747', '380'] }
+          }
+        }
+      ],
+      as: 'rotasCorrespondentes'
+    }
+  },
+  { $addFields: { totalRotas: { $size: "$rotasCorrespondentes" } } },
+  {
+    $group: {
+      _id: "$name",
+      totalRotas: { $sum: "$totalRotas" }
+    }
+  },
+  { $sort: { totalRotas: -1 } },
+  { $limit: 1 }
+]);
