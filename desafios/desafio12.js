8417dad72e1ffc77d15988a049dc0f2e8ecb1d3a
lauro <laurolyra@gmail.com>
2020-06-22 11:25:02 -0300

    Lauro mongoDB Aggregations

---
+++ desafios/desafio12.js
@@ -0,0 +1,20 @@
+use aggregations;
+
+db.trips.aggregate([
+  { $match: { $expr: { $eq: [{ $dayOfWeek: "$startTime" }, 5] } } },
+  {
+    $group: {
+      _id: "$startStationName",
+      total: { $sum: 1 }
+    }
+  },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+  {
+    $project: {
+      _id: 0,
+      nomeEstacao: "$_id",
+      total: "1"
+    }
+  }
+]);
