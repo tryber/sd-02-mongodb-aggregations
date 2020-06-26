d5675047f9d793477c4ef27f2b4a855a5163299e
Mateus Talles <mateustalles@gmail.com>
2020-06-26 17:00:45 -0300

    Desafio 8 correcao

--- desafio8.js
+++ desafio8.js
@@ -9,19 +9,16 @@ db.air_alliances.aggregate([
       pipeline: [
         {
           $match: {
-            $expr: {
-                $and:[
-                { $eq: [ "$$airline", "$airline.name" ] },
-                { $in: [ "$airplane", [ "747", "380" ] ] }
-              ]
-            }
+            $expr: { $eq: [ "$$airline", "$airline.name" ] },
+            airplane: { $in: ["747", "380"] }
           }
-        }
+        },
       ],
       as: "routes"
     }
   },
-  { $group: { _id: "$name", totalRotas: { $sum: 1 } } },
+  { $match: { routes: { $ne: [] } } },
+  { $group: { _id: "$name", totalRotas: { $sum: { $size: "$routes" } } } },
   { $sort: { totalRotas: -1 } },
   { $limit: 1 }
 ]).pretty();
