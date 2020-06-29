8be19e2f37a56c5982f7dbad07315bd9187c8ba7
Andrey <andreytsuzuki@gmail.com>
2020-06-29 18:38:01 -0300

    fix

--- desafio8.js
+++ desafio8.js
@@ -1,2 +1,38 @@
 use aggregations;
 
+db.air_alliances.aggregate([
+  {
+    $unwind: "$airlines",
+  },
+  {
+    $lookup:
+    {
+      from: "air_routes",
+      let: { route_airline: "$airlines" },
+      pipeline: [{
+          $match: {
+            $expr: { $eq: ["$airline.name", "$$route_airline"] },
+            airplane: { $in: ['747', '380'] }
+          }
+        }],
+      as: "alliances_routes"
+    }
+  },
+  {
+    $match: {
+      "alliances_routes": { $not: { $size: 0 } }
+    }
+  },
+  {
+    $group: {
+      _id: "$name",
+      totalRotas: { $sum: { $size: "$alliances_routes" } },
+    }
+  },
+  {
+    $sort: { "totalRotas": -1 }
+  },
+  {
+    $limit: 1
+  }
+]);
